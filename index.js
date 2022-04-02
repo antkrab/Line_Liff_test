f846d6326370fae746a5ac3ea0d68a4868bacd03
antkrab <intuch68@gmail.com>
2022-04-02 14:30:32 +0700

    2 files modified

--- index.js
+++ index.js
@@ -26,7 +26,113 @@ const friendShip = document.getElementById("friendShip")
 
 async function main() {
   // Initialize LIFF app)
-  
+  await liff.init({ liffId: "1657025258-kYRvbXne" })
   // Try a LIFF function
+  switch (liff.getOS()) {
+    case "android": body.style.backgroundColor = "#d1f5d3"; break;
+    case "ios": body.style.backgroundColor = "#eeeeee"; break;
+  }
+  if (!liff.isInClient()) {
+    if (liff.isLoggedIn()) {
+      getUserProfile();
+      btnShare.style.display = "block"
+      btnLogIn.style.display = "none"
+      btnLogOut.style.display = "block"
+      btnScanCode.style.display = "block"
+      getFriendship();
+    } else {
+      btnLogIn.style.display = "block"
+      btnLogOut.style.display = "none"
+    }
+  }
+  else{
+    getUserProfile();
+    btnShare.style.display = "block"
+    btnSend.style.display = "block"
+    btnScanCode.style.display = "block"
+    getFriendship();
+  }
+  btnOpenWindow.style.display = "block"
 }
 main()
+
+async function getUserProfile() {
+  const profile = await liff.getProfile()
+  pictureUrl.src = profile.pictureUrl
+  userId.innerHTML = "<b>userId:</b> " + profile.userId
+  statusMessage.innerHTML = "<b>statusMessage:</b> " + profile.statusMessage
+  displayName.innerHTML = "<b>displayName:</b> " + profile.displayName
+  email.innerHTML = "<b>email:</b> " + liff.getDecodedIDToken().email
+}
+
+btnLogIn.onclick = () => {
+  liff.login();
+}
+
+btnLogOut.onclick = () => {
+  liff.logout();
+  window.location.reload();
+}
+
+async function sendMsg() {
+  if (liff.getContext().type !== "none" && liff.getContext().type !== "external") {
+    await liff.sendMessages([
+      {
+        "type": "text",
+        "text": "This message was sent by sendMessages()"
+      },
+      {
+        "type": "text",
+        "text": "LINE Developers University Workshop!"
+      }
+    ]);
+    liff.closeWindow()
+  }
+}
+
+btnSend.onclick = () => {
+  sendMsg();
+}
+
+async function shareMsg() {
+  await liff.shareTargetPicker([
+    {
+      type: "image",
+      originalContentUrl: "https://d.line-scdn.net/stf/line-lp/2016_en_02.jpg",
+      previewImageUrl: "https://d.line-scdn.net/stf/line-lp/2016_en_02.jpg"
+    },
+    {
+      "type": "text",
+      "text": "LINE Developers University Workshop!"
+    }
+  ])
+}
+
+btnShare.onclick = () => {
+  shareMsg();
+}
+
+async function scanCode() {
+  const result = await liff.scanCodeV2()
+  code.innerHTML = "<b>Code: </b>" + result.value
+}
+
+btnScanCode.onclick = () => {
+  scanCode()
+}
+
+btnOpenWindow.onclick = () => {
+  liff.openWindow({
+    url: "https://codelab.line.me/",
+    external: true
+  });
+}
+
+async function getFriendship() {
+  let msg = "Hooray! You and our chatbot are friend."
+  const friend = await liff.getFriendship()
+  if (!friend.friendFlag) {
+     msg = "<a href=\"https://line.me/R/ti/p/@551pdicj\">Follow our chatbot here!</a>";
+  }
+  friendShip.innerHTML = msg;
+}
